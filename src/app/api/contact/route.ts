2959a37220eac5d465b1e3f7fd2f02b9e3da74d3
akshay <akshay.shinde@ly.design>
2025-05-12 16:33:42 +0530

    changed api endpoints

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,37 +1,48 @@
-import { payload } from '@/payload'
+import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
   try {
-    const data = await req.json()
+    const data = await req.json();
 
     // The created Post document is returned
-    await payload.create({
-      collection: 'contact', // required
-      data: {
-        ...data,
+    await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/contact`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    })
+      body: JSON.stringify({ ...data }),
+    });
 
-    await payload.sendEmail({
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 587, // or 465 if using secure SSL
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
       to: data?.email,
       subject: `New Contact Inquiry from ${data?.email}`,
       text: `
       Hello,
-      
+
       You have received a new inquiry with the following details:
-      
-      - Name: ${data?.name ?? 'NA'}
-      - Mobile: ${data?.mobile ?? 'NA'}
-      - Email: ${data?.email ?? 'NA'}
-      - Destination: ${data?.destination ?? 'NA'}
-      - Ideas: ${data?.ideas ?? 'NA'}
-      
+
+      - Name: ${data?.name ?? "NA"}
+      - Mobile: ${data?.mobile ?? "NA"}
+      - Email: ${data?.email ?? "NA"}
+      - Destination: ${data?.destination ?? "NA"}
+      - Ideas: ${data?.ideas ?? "NA"}
+
       Please review and follow up as needed.
       `,
-    })
+    });
 
-    return Response.json({ message: 'Email sent successfully' })
+    return Response.json({ message: "Email sent successfully" });
   } catch (error) {
-    return Response.json({ error: 'Failed to send email' })
+    return Response.json({ error: "Failed to send email", ERR: error });
   }
 }
--- src/app/api/inquiry/route.ts
+++ src/app/api/inquiry/route.ts
@@ -1,18 +1,29 @@
-import { payload } from '@/payload'
+import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
   try {
-    const data = await req.json()
+    const data = await req.json();
 
     // The created Post document is returned
-    await payload.create({
-      collection: 'inquiry', // required
-      data: {
-        ...data,
+    await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/inquiry`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    })
+      body: JSON.stringify({ ...data }),
+    });
 
-    await payload.sendEmail({
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 587, // or 465 if using secure SSL
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
       to: data?.email,
       subject: `New Inquiry for ${data?.destination}`,
       text: `
@@ -20,19 +31,19 @@ export async function POST(req: Request) {
       
       You have received a new inquiry with the following details:
       
-      - Name: ${data?.name ?? 'NA'}
-      - Mobile: ${data?.mobile ?? 'NA'}
-      - Email: ${data?.email ?? 'NA'}
-      - No. of travellers: ${data?.travellers ?? 'NA'}
-      - Destination: ${data?.destination?.replaceAll('-', ' ') ?? 'NA'}
-      - Type: ${data?.type?.replaceAll('-', ' ') ?? 'NA'}
+      - Name: ${data?.name ?? "NA"}
+      - Mobile: ${data?.mobile ?? "NA"}
+      - Email: ${data?.email ?? "NA"}
+      - No. of travellers: ${data?.travellers ?? "NA"}
+      - Destination: ${data?.destination?.replaceAll("-", " ") ?? "NA"}
+      - Type: ${data?.type?.replaceAll("-", " ") ?? "NA"}
       
       Please review and follow up as needed.
       `,
-    })
+    });
 
-    return Response.json({ message: 'Email sent successfully' })
+    return Response.json({ message: "Email sent successfully" });
   } catch (error) {
-    return Response.json({ error: 'Failed to send email' })
+    return Response.json({ error: "Failed to send email", ERR: error });
   }
 }
